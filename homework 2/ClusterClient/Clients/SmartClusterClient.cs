bcd4a57f5297f29277b77dd11f29a01206864b39
Matvey <rusinovmatvei@gmail.com>
2026-03-03 20:27:12 +0500

    implemented smart client with performance tracker

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,23 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class SmartClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public class SmartClusterClient : ClusterClientBase
+    private readonly ReplicaPerformanceTracker tracker = new();
+
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-        public SmartClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        var stopwatch = Stopwatch.StartNew();
+
+        var orderedReplicas = tracker.OrderByFastest(ReplicaAddresses);
+        var remainingReplicas = orderedReplicas.Length;
+
+        var inFlight = new List<(string replica, Task<string> task, Stopwatch sw)>();
+
+        foreach (var replica in orderedReplicas)
         {
+            var remainingTime = timeout - stopwatch.Elapsed;
+            if (remainingTime <= TimeSpan.Zero)
+                break;
+
+            var timeoutPerRequest = remainingTime.Divide(remainingReplicas);
+            remainingReplicas--;
+
+            var request = CreateRequest(replica + "?query=" + query);
+            Log.InfoFormat($"Processing {request.RequestUri}");
+
+            var sw = Stopwatch.StartNew();
+            var task = ProcessRequestAsync(request);
+
+            inFlight.Add((replica, task, sw));
+                
+            var delayTask = Task.Delay(timeoutPerRequest);
+            var anyResponseTask = Task.WhenAny(inFlight.Select(x => x.task));
+
+            var completed = await Task.WhenAny(anyResponseTask, delayTask);
+
+            if (completed != anyResponseTask) continue;
+            {
+                await anyResponseTask;
+                    
+                var winner = inFlight.First(x => x.task.IsCompleted);
+
+                try
+                {
+                    var result = await winner.task;
+                    winner.sw.Stop();
+                    tracker.ReportResult(winner.replica, winner.sw.ElapsedMilliseconds);
+                    return result;
+                }
+                catch (WebException)
+                {
+                    winner.sw.Stop();
+                    tracker.ReportResult(winner.replica, winner.sw.ElapsedMilliseconds);
+                    inFlight.Remove(winner);
+                }
+            }
+        }
+            
+        while (inFlight.Count > 0)
+        {
+            var remainingTime = timeout - stopwatch.Elapsed;
+            if (remainingTime <= TimeSpan.Zero)
+                break;
+
+            var completed = await Task.WhenAny(
+                Task.WhenAny(inFlight.Select(x => x.task)),
+                Task.Delay(remainingTime)
+            );
+
+            if (completed is Task<string>)
+            {
+                var winner = inFlight.First(x => x.task.IsCompleted);
+
+                try
+                {
+                    var result = await winner.task;
+                    winner.sw.Stop();
+                    tracker.ReportResult(winner.replica, winner.sw.ElapsedMilliseconds);
+                    return result;
+                }
+                catch (WebException)
+                {
+                    winner.sw.Stop();
+                    tracker.ReportResult(winner.replica, winner.sw.ElapsedMilliseconds);
+                    inFlight.Remove(winner);
+                }
+            }
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
+        throw new TimeoutException("No replica responded within timeout");
     }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
 }
